ca564203de0da05c9eaa121ac4993a8fddbc2da5
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-03-14 19:30:50 +0100

    feat: implementation makeWord function (Fri, March 14, 2025 19:30:35 PM)

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -126,8 +126,16 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const lengthArr = Math.max(...Object.keys(lettersObject));
+  const arr = Array.from({ length: lengthArr }).fill(0);
+  Object.keys(lettersObject).forEach((key) => {
+    lettersObject[key].forEach((pos) => {
+      arr[pos] = key;
+    });
+  });
+
+  return arr.join('');
 }
 
 /**
